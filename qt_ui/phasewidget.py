fd26ac9b2b3d257a534257520dbaf0dc41763f63
diglet48 <diglet48@gmail.com>
2023-03-10 13:05:41 +0100

    fix resource path when running from source

--- qt_ui/phasewidget.py
+++ qt_ui/phasewidget.py
@@ -13,7 +13,7 @@ from qt_ui.stim_config import PositionParameters
 
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
-    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    base_path = getattr(sys, '_MEIPASS', os.path.join(os.path.dirname(os.path.abspath(__file__)), os.path.pardir))
     return os.path.join(base_path, relative_path)
 
 
