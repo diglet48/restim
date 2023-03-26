02e0a83a6a2011e860f0084eee74822d0e2ff1a2
diglet48 <diglet48@gmail.com>
2023-03-26 13:36:35 +0200

    add option to export to mp3 in funscript_to_wav.py

--- funscript_to_wav.py
+++ funscript_to_wav.py
@@ -1,6 +1,7 @@
 import numpy as np
 import argparse
-import scipy
+import re
+import pydub
 import json
 import time
 
@@ -33,13 +34,15 @@ def parse_funscript(path):
     return np.array(x, dtype=np.float32), np.array(y, dtype=np.float32)
 
 
-def export_wav(filename, sample_rate, channel1, channel2=None):
-    if channel2 is None:
-        a = channel1.astype(np.float32)
-    else:
-        a = np.vstack((channel1.astype(np.float32), channel2.astype(np.float32))).transpose()
+def export_audio_to_file(filename, samplerate, channel1, channel2, format):
+    def audio_to_bytes(L, R):
+        data = np.vstack((L, R)).transpose()
+        data = np.clip(data * 2 ** 15, -32768, 32767).astype(np.int16)
+        return data.tobytes()
 
-    scipy.io.wavfile.write(filename, sample_rate, a)
+    segment = pydub.AudioSegment(data=audio_to_bytes(channel1, channel2),
+                                 sample_width=2, frame_rate=samplerate, channels=2)
+    segment.export(filename, format=format)
 
 
 if __name__ == '__main__':
@@ -47,23 +50,37 @@ if __name__ == '__main__':
         prog='funscript conversion',
         description='convert 1d funscript into 2d')
 
-    parser.add_argument('-a', help='input funscript (alpha axis)', required=True)
-    parser.add_argument('-b', help='input funsciript (beta axis, optional)')
-    parser.add_argument('-c')
+    parser.add_argument('-a', '--alpha', help='input funscript (alpha axis)', required=True)
+    parser.add_argument('-b', '--beta', help='input funsciript (beta axis, optional)')
 
-    parser.add_argument('-o', '--out', required=True)
+    parser.add_argument('-o', '--out', help='output sound file, default same as funscript')
+    parser.add_argument('--format', help='mp3 (default) or wav')
     parser.add_argument('--sample_rate', default=44100)
     parser.add_argument('--frequency', default=900)
 
     args = parser.parse_args()
     start_time = time.time()
 
+    # determine format
+    format = args.format
+    if format is None:
+        if args.out is not None:
+            format = re.search('\.(.+)$', args.out).group(1)  # detect format from output
+        else:
+            format = 'mp3'  # default to mp3
+
+    # determine output path
+    output = args.out
+    if output is None:
+        output = re.sub('(\.alpha)?\.funscript$', '', args.alpha) # strip extension
+        output = output + '.' + format
+
     print('parsing funscripts')
-    print(args.a)
-    x1, y1 = parse_funscript(args.a)
-    if args.b:
-        print(args.b)
-        x2, y2 = parse_funscript(args.b)
+    print(args.alpha)
+    x1, y1 = parse_funscript(args.alpha)
+    if args.beta:
+        print(args.beta)
+        x2, y2 = parse_funscript(args.beta)
     else:
         print("1d-to-2d funscript conversion")
         x1, y1, y2 = funscript_1d_to_2d.convert_funscript_radial((x1, y1))
@@ -82,7 +99,7 @@ if __name__ == '__main__':
     print('generate audio (may take a while)')
     L, R = generate_more(timeline, args.frequency, alpha, beta)
 
-    print('export {}'.format(args.out))
-    export_wav(args.out, args.sample_rate, L, R)
+    print('export {}'.format(output))
+    export_audio_to_file(output, args.sample_rate, L, R, format=format)
 
     print('took {:.2f} seconds'.format(time.time() - start_time))
