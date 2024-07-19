Fix division by zero error caused by incorrect fps calculation.

qt_ui/widgets/phase_widget.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 import matplotlib
 import time
 import math
+import numpy as np
 
 from PyQt5.QtCore import QPoint, QPointF
 from PyQt5.QtWidgets import QGraphicsView, QGraphicsEllipseItem
@@ -108,7 +109,7 @@ class PhaseWidgetWithPoint(PhaseWidgetBase):
         self.scene.addItem(self.dot)
 
     def refreshSettings(self):
-        self.timer.setInterval(int(1000 // settings.display_latency.get()))
+        self.timer.setInterval(int(1000 // np.clip(settings.display_fps.get(), 1.0, 500.0)))
         self.latency = settings.display_latency.get() / 1000.0
 
     def mouse_event(self, alpha, beta, buttons: QtCore.Qt.MouseButtons):
@@ -437,7 +438,7 @@ class PhaseWidgetCalibration(PhaseWidgetBase):
         self.last_state = None
 
     def refreshSettings(self):
-        self.timer.setInterval(int(1000 // settings.display_latency.get()))
+        self.timer.setInterval(int(1000 // np.clip(settings.display_fps.get(), 1.0, 500.0)))
         self.latency = settings.display_latency.get() / 1000.0
 
     def set_axis(self, neutral: AbstractAxis, right: AbstractAxis):