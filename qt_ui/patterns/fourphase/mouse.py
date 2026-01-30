diglet48/restim

commit 5850fee87d984c012ac9b5d6edba9f2c554d837e

Improve support for lsm6dsox IMU in FOC-V4

qt_ui/patterns/fourphase/mouse.py
@@ -27,6 +27,3 @@ class MousePattern(FourphasePattern):
 
     def update(self, dt: float):
         return self.x, self.y, self.z
-
-    def last_position_is_mouse_position(self):
-        return (self.x, self.y) == (self.alpha.last_value(), self.beta.last_value())

qt_ui/patterns/fourphase_patterns.py
@@ -86,13 +86,10 @@ class FourphaseMotionGenerator(QtCore.QObject):
 
         if not self.any_scripts_loaded():
             if isinstance(self.pattern, MousePattern):
-                if self.pattern.last_position_is_mouse_position():
-                    pass
-                else:
-                    a = self.alpha.interpolate(time.time() - self.latency)
-                    b = self.beta.interpolate(time.time() - self.latency)
-                    c = self.gamma.interpolate(time.time() - self.latency)
-                    self.position_updated.emit(a, b, c)
+                a = self.alpha.last_value()
+                b = self.beta.last_value()
+                c = self.gamma.last_value()
+                self.position_updated.emit(a, b, c)
             else:
                 a, b, c = self.pattern.update(dt * self.velocity)
                 self.alpha.add(a)

qt_ui/patterns/threephase/mouse.py
@@ -24,7 +24,3 @@ class MousePattern(ThreephasePattern):
         self.y = y
     def update(self, dt: float):
         return self.x * self.amplitude, self.y * self.amplitude
-    def last_position_is_mouse_position(self):
-        if self.alpha is not None and self.beta is not None:
-            return (self.x, self.y) == (self.alpha.last_value(), self.beta.last_value())
-        return False

qt_ui/patterns/threephase_patterns.py
@@ -101,12 +101,9 @@ class ThreephaseMotionGenerator(QtCore.QObject):
 
         if not self.any_scripts_loaded():
             if isinstance(self.pattern, MousePattern):
-                if self.pattern.last_position_is_mouse_position():
-                    pass
-                else:
-                    a = self.alpha.interpolate(time.time() - self.latency)
-                    b = self.beta.interpolate(time.time() - self.latency)
-                    self.position_updated.emit(a, b)
+                a = self.alpha.last_value()
+                b = self.beta.last_value()
+                self.position_updated.emit(a, b)
             else:
                 a, b = self.pattern.update(dt * self.velocity)
                 self.alpha.add(a)

designer/imusettingswidget.ui
@@ -1,0 +1,209 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<ui version="4.0">
+ <class>IMUSettingsWidget</class>
+ <widget class="QWidget" name="IMUSettingsWidget">
+  <property name="geometry">
+   <rect>
+    <x>0</x>
+    <y>0</y>
+    <width>614</width>
+    <height>644</height>
+   </rect>
+  </property>
+  <property name="windowTitle">
+   <string>Form</string>
+  </property>
+  <layout class="QVBoxLayout" name="verticalLayout">
+   <item>
+    <widget class="QGroupBox" name="groupBox">
+     <property name="sizePolicy">
+      <sizepolicy hsizetype="Preferred" vsizetype="Fixed">
+       <horstretch>0</horstretch>
+       <verstretch>0</verstretch>
+      </sizepolicy>
+     </property>
+     <property name="title">
+      <string>Movement</string>
+     </property>
+     <layout class="QFormLayout" name="formLayout">
+      <item row="0" column="0">
+       <widget class="QLabel" name="label">
+        <property name="text">
+         <string>Device amplitude [cm]</string>
+        </property>
+       </widget>
+      </item>
+      <item row="0" column="1">
+       <widget class="QDoubleSpinBox" name="doubleSpinBox_movement_in">
+        <property name="decimals">
+         <number>1</number>
+        </property>
+        <property name="minimum">
+         <double>-100.000000000000000</double>
+        </property>
+        <property name="singleStep">
+         <double>0.100000000000000</double>
+        </property>
+       </widget>
+      </item>
+      <item row="1" column="1">
+       <widget class="QDoubleSpinBox" name="doubleSpinBox_movement_out">
+        <property name="decimals">
+         <number>1</number>
+        </property>
+        <property name="minimum">
+         <double>-1.000000000000000</double>
+        </property>
+        <property name="maximum">
+         <double>1.000000000000000</double>
+        </property>
+        <property name="singleStep">
+         <double>0.100000000000000</double>
+        </property>
+       </widget>
+      </item>
+      <item row="1" column="0">
+       <widget class="QLabel" name="label_2">
+        <property name="text">
+         <string>Alpha amplitude</string>
+        </property>
+       </widget>
+      </item>
+     </layout>
+    </widget>
+   </item>
+   <item>
+    <widget class="QGroupBox" name="groupBox_2">
+     <property name="sizePolicy">
+      <sizepolicy hsizetype="Preferred" vsizetype="Fixed">
+       <horstretch>0</horstretch>
+       <verstretch>0</verstretch>
+      </sizepolicy>
+     </property>
+     <property name="title">
+      <string>Velocity</string>
+     </property>
+     <layout class="QFormLayout" name="formLayout_2">
+      <item row="0" column="0">
+       <widget class="QLabel" name="label_3">
+        <property name="text">
+         <string>Device speed [cm/s]</string>
+        </property>
+       </widget>
+      </item>
+      <item row="1" column="0">
+       <widget class="QLabel" name="label_4">
+        <property name="toolTip">
+         <string>positive: increase when moving
+negative: decrease when moving</string>
+        </property>
+        <property name="text">
+         <string>Intensity increase [%] (?)</string>
+        </property>
+       </widget>
+      </item>
+      <item row="0" column="1">
+       <widget class="QDoubleSpinBox" name="doubleSpinBox_velocity_in">
+        <property name="decimals">
+         <number>0</number>
+        </property>
+        <property name="minimum">
+         <double>0.000000000000000</double>
+        </property>
+       </widget>
+      </item>
+      <item row="1" column="1">
+       <widget class="QDoubleSpinBox" name="doubleSpinBox_intensity_increase">
+        <property name="minimum">
+         <double>-100.000000000000000</double>
+        </property>
+        <property name="maximum">
+         <double>100.000000000000000</double>
+        </property>
+        <property name="singleStep">
+         <double>0.100000000000000</double>
+        </property>
+       </widget>
+      </item>
+     </layout>
+    </widget>
+   </item>
+   <item>
+    <widget class="QGroupBox" name="groupBox_3">
+     <property name="sizePolicy">
+      <sizepolicy hsizetype="Preferred" vsizetype="Preferred">
+       <horstretch>0</horstretch>
+       <verstretch>0</verstretch>
+      </sizepolicy>
+     </property>
+     <property name="maximumSize">
+      <size>
+       <width>16777215</width>
+       <height>16777215</height>
+      </size>
+     </property>
+     <property name="title">
+      <string>Live update</string>
+     </property>
+     <layout class="QVBoxLayout" name="verticalLayout_2">
+      <item>
+       <widget class="QCheckBox" name="checkBox">
+        <property name="text">
+         <string>update</string>
+        </property>
+       </widget>
+      </item>
+      <item>
+       <widget class="PlotWidget" name="graph" native="true">
+        <property name="sizePolicy">
+         <sizepolicy hsizetype="Preferred" vsizetype="Maximum">
+          <horstretch>0</horstretch>
+          <verstretch>100</verstretch>
+         </sizepolicy>
+        </property>
+        <property name="minimumSize">
+         <size>
+          <width>0</width>
+          <height>50</height>
+         </size>
+        </property>
+        <property name="maximumSize">
+         <size>
+          <width>16777215</width>
+          <height>300</height>
+         </size>
+        </property>
+       </widget>
+      </item>
+      <item>
+       <spacer name="verticalSpacer">
+        <property name="orientation">
+         <enum>Qt::Orientation::Vertical</enum>
+        </property>
+        <property name="sizeType">
+         <enum>QSizePolicy::Policy::Expanding</enum>
+        </property>
+        <property name="sizeHint" stdset="0">
+         <size>
+          <width>20</width>
+          <height>1</height>
+         </size>
+        </property>
+       </spacer>
+      </item>
+     </layout>
+    </widget>
+   </item>
+  </layout>
+ </widget>
+ <customwidgets>
+  <customwidget>
+   <class>PlotWidget</class>
+   <extends>QWidget</extends>
+   <header>pyqtgraph.h</header>
+   <container>1</container>
+  </customwidget>
+ </customwidgets>
+ <resources/>
+ <connections/>
+</ui>

designer/mainwindow.ui
@@ -7,7 +7,7 @@
     <x>0</x>
     <y>0</y>
     <width>843</width>
-    <height>663</height>
+    <height>657</height>
    </rect>
   </property>
   <property name="windowTitle">
@@ -237,7 +237,7 @@
            <bool>true</bool>
           </property>
           <property name="currentIndex">
-           <number>6</number>
+           <number>8</number>
           </property>
           <widget class="ThreePhaseSettingsWidget" name="tab_threephase">
            <attribute name="title">
@@ -289,6 +289,46 @@
        </layout>
       </widget>
       <widget class="MediaSettingsWidget" name="page_media"/>
+      <widget class="QWidget" name="page_sensors">
+       <layout class="QHBoxLayout" name="horizontalLayout_3">
+        <property name="topMargin">
+         <number>0</number>
+        </property>
+        <property name="bottomMargin">
+         <number>0</number>
+        </property>
+        <item>
+         <widget class="QTabWidget" name="tabWidget_2">
+          <property name="currentIndex">
+           <number>0</number>
+          </property>
+          <widget class="IMUSettingsWidget" name="tab_imu">
+           <attribute name="title">
+            <string>IMU</string>
+           </attribute>
+          </widget>
+          <widget class="QWidget" name="tab_AS5311">
+           <attribute name="title">
+            <string>AS5311</string>
+           </attribute>
+           <widget class="QLabel" name="label">
+            <property name="geometry">
+             <rect>
+              <x>20</x>
+              <y>30</y>
+              <width>131</width>
+              <height>16</height>
+             </rect>
+            </property>
+            <property name="text">
+             <string>Not implemented yet</string>
+            </property>
+           </widget>
+          </widget>
+         </widget>
+        </item>
+       </layout>
+      </widget>
      </widget>
     </item>
    </layout>
@@ -353,6 +393,7 @@
    </attribute>
    <addaction name="actionControl"/>
    <addaction name="actionMedia"/>
+   <addaction name="actionSensors"/>
    <addaction name="actionStart"/>
   </widget>
   <action name="actionFunscript_conversion">
@@ -419,6 +460,9 @@
    </property>
   </action>
   <action name="actionStart">
+   <property name="checkable">
+    <bool>false</bool>
+   </property>
    <property name="icon">
     <iconset resource="../resources/restim.qrc">
      <normaloff>:/restim/play_poly.svg</normaloff>:/restim/play_poly.svg</iconset>
@@ -460,6 +504,21 @@
   <action name="actionAbout">
    <property name="text">
     <string>About</string>
+   </property>
+  </action>
+  <action name="actionSensors">
+   <property name="checkable">
+    <bool>true</bool>
+   </property>
+   <property name="icon">
+    <iconset resource="../resources/restim.qrc">
+     <normaloff>:/restim/activity-1_poly.svg</normaloff>:/restim/activity-1_poly.svg</iconset>
+   </property>
+   <property name="text">
+    <string>Sensors</string>
+   </property>
+   <property name="shortcut">
+    <string>Ctrl+3</string>
    </property>
   </action>
  </widget>
@@ -537,6 +596,12 @@
    <class>FourPhaseSettingsWidget</class>
    <extends>QWidget</extends>
    <header>qt_ui/four_phase_settings_widget.h</header>
+   <container>1</container>
+  </customwidget>
+  <customwidget>
+   <class>IMUSettingsWidget</class>
+   <extends>QWidget</extends>
+   <header>qt_ui/imu_settings_widget.h</header>
    <container>1</container>
   </customwidget>
  </customwidgets>

device/focstim/focstim_rpc_pb2.py
@@ -9,7 +9,7 @@ _sym_db = _symbol_database.Default()
 from . import notifications_pb2 as notifications__pb2
 from . import messages_pb2 as messages__pb2
 from . import constants_pb2 as constants__pb2
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x11focstim_rpc.proto\x12\x0bfocstim_rpc\x1a\x13notifications.proto\x1a\x0emessages.proto\x1a\x0fconstants.proto"\xd3\x06\n\x0cNotification\x12:\n\x11notification_boot\x18\x01 \x01(\x0b2\x1d.focstim_rpc.NotificationBootH\x00\x12L\n\x1anotification_potentiometer\x18\x02 \x01(\x0b2&.focstim_rpc.NotificationPotentiometerH\x00\x12B\n\x15notification_currents\x18\x03 \x01(\x0b2!.focstim_rpc.NotificationCurrentsH\x00\x12Q\n\x1dnotification_model_estimation\x18\x04 \x01(\x0b2(.focstim_rpc.NotificationModelEstimationH\x00\x12I\n\x19notification_system_stats\x18\x05 \x01(\x0b2$.focstim_rpc.NotificationSystemStatsH\x00\x12I\n\x19notification_signal_stats\x18\x06 \x01(\x0b2$.focstim_rpc.NotificationSignalStatsH\x00\x12@\n\x14notification_battery\x18\x07 \x01(\x0b2 .focstim_rpc.NotificationBatteryH\x00\x12B\n\x15notification_lsm6dsox\x18\x08 \x01(\x0b2!.focstim_rpc.NotificationLSM6DSOXH\x00\x12J\n\x19notification_debug_string\x18\xe8\x07 \x01(\x0b2$.focstim_rpc.NotificationDebugStringH\x00\x12J\n\x19notification_debug_as5311\x18\xe9\x07 \x01(\x0b2$.focstim_rpc.NotificationDebugAS5311H\x00\x12J\n\x19notification_debug_edging\x18\xea\x07 \x01(\x0b2$.focstim_rpc.NotificationDebugEdgingH\x00\x12\x12\n\ttimestamp\x18\xe7\x07 \x01(\x04B\x0e\n\x0cnotification"\xb4\x06\n\x07Request\x12\n\n\x02id\x18\x01 \x01(\r\x12H\n\x18request_firmware_version\x18\xf4\x03 \x01(\x0b2#.focstim_rpc.RequestFirmwareVersionH\x00\x12H\n\x18request_capabilities_get\x18\xf5\x03 \x01(\x0b2#.focstim_rpc.RequestCapabilitiesGetH\x00\x12@\n\x14request_signal_start\x18\xf6\x03 \x01(\x0b2\x1f.focstim_rpc.RequestSignalStartH\x00\x12>\n\x13request_signal_stop\x18\xf7\x03 \x01(\x0b2\x1e.focstim_rpc.RequestSignalStopH\x00\x12>\n\x14request_axis_move_to\x18\x05 \x01(\x0b2\x1e.focstim_rpc.RequestAxisMoveToH\x00\x12B\n\x15request_timestamp_set\x18\xf8\x03 \x01(\x0b2 .focstim_rpc.RequestTimestampSetH\x00\x12B\n\x15request_timestamp_get\x18\xf9\x03 \x01(\x0b2 .focstim_rpc.RequestTimestampGetH\x00\x12M\n\x1brequest_wifi_parameters_set\x18\xfb\x03 \x01(\x0b2%.focstim_rpc.RequestWifiParametersSetH\x00\x12=\n\x13request_wifi_ip_get\x18\xfc\x03 \x01(\x0b2\x1d.focstim_rpc.RequestWifiIPGetH\x00\x12R\n\x1erequest_debug_stm32_deep_sleep\x18\xe8\x07 \x01(\x0b2\'.focstim_rpc.RequestDebugStm32DeepSleepH\x00\x12S\n\x1erequest_debug_enter_bootloader\x18\xe9\x07 \x01(\x0b2(.focstim_rpc.RequestDebugEnterBootloaderH\x00B\x08\n\x06params"\x97\x06\n\x08Response\x12\n\n\x02id\x18\x01 \x01(\r\x12J\n\x19response_firmware_version\x18\xf4\x03 \x01(\x0b2$.focstim_rpc.ResponseFirmwareVersionH\x00\x12J\n\x19response_capabilities_get\x18\xf5\x03 \x01(\x0b2$.focstim_rpc.ResponseCapabilitiesGetH\x00\x12B\n\x15response_signal_start\x18\xf6\x03 \x01(\x0b2 .focstim_rpc.ResponseSignalStartH\x00\x12@\n\x14response_signal_stop\x18\xf7\x03 \x01(\x0b2\x1f.focstim_rpc.ResponseSignalStopH\x00\x12@\n\x15response_axis_move_to\x18\x05 \x01(\x0b2\x1f.focstim_rpc.ResponseAxisMoveToH\x00\x12D\n\x16response_timestamp_set\x18\xf8\x03 \x01(\x0b2!.focstim_rpc.ResponseTimestampSetH\x00\x12D\n\x16response_timestamp_get\x18\xf9\x03 \x01(\x0b2!.focstim_rpc.ResponseTimestampGetH\x00\x12O\n\x1cresponse_wifi_parameters_set\x18\xfb\x03 \x01(\x0b2&.focstim_rpc.ResponseWifiParametersSetH\x00\x12?\n\x14response_wifi_ip_get\x18\xfc\x03 \x01(\x0b2\x1e.focstim_rpc.ResponseWifiIPGetH\x00\x12T\n\x1fresponse_debug_stm32_deep_sleep\x18\xe8\x07 \x01(\x0b2(.focstim_rpc.ResponseDebugStm32DeepSleepH\x00\x12!\n\x05error\x18\x03 \x01(\x0b2\x12.focstim_rpc.ErrorB\x08\n\x06result"*\n\x05Error\x12!\n\x04code\x18\x01 \x01(\x0e2\x13.focstim_rpc.Errors"\x9e\x01\n\nRpcMessage\x12\'\n\x07request\x18\x02 \x01(\x0b2\x14.focstim_rpc.RequestH\x00\x12)\n\x08response\x18\x04 \x01(\x0b2\x15.focstim_rpc.ResponseH\x00\x121\n\x0cnotification\x18\x05 \x01(\x0b2\x19.focstim_rpc.NotificationH\x00B\t\n\x07messageb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x11focstim_rpc.proto\x12\x0bfocstim_rpc\x1a\x13notifications.proto\x1a\x0emessages.proto\x1a\x0fconstants.proto"\xd3\x06\n\x0cNotification\x12:\n\x11notification_boot\x18\x01 \x01(\x0b2\x1d.focstim_rpc.NotificationBootH\x00\x12L\n\x1anotification_potentiometer\x18\x02 \x01(\x0b2&.focstim_rpc.NotificationPotentiometerH\x00\x12B\n\x15notification_currents\x18\x03 \x01(\x0b2!.focstim_rpc.NotificationCurrentsH\x00\x12Q\n\x1dnotification_model_estimation\x18\x04 \x01(\x0b2(.focstim_rpc.NotificationModelEstimationH\x00\x12I\n\x19notification_system_stats\x18\x05 \x01(\x0b2$.focstim_rpc.NotificationSystemStatsH\x00\x12I\n\x19notification_signal_stats\x18\x06 \x01(\x0b2$.focstim_rpc.NotificationSignalStatsH\x00\x12@\n\x14notification_battery\x18\x07 \x01(\x0b2 .focstim_rpc.NotificationBatteryH\x00\x12B\n\x15notification_lsm6dsox\x18\x08 \x01(\x0b2!.focstim_rpc.NotificationLSM6DSOXH\x00\x12J\n\x19notification_debug_string\x18\xe8\x07 \x01(\x0b2$.focstim_rpc.NotificationDebugStringH\x00\x12J\n\x19notification_debug_as5311\x18\xe9\x07 \x01(\x0b2$.focstim_rpc.NotificationDebugAS5311H\x00\x12J\n\x19notification_debug_edging\x18\xea\x07 \x01(\x0b2$.focstim_rpc.NotificationDebugEdgingH\x00\x12\x12\n\ttimestamp\x18\xe7\x07 \x01(\x04B\x0e\n\x0cnotification"\xbe\x07\n\x07Request\x12\n\n\x02id\x18\x01 \x01(\r\x12H\n\x18request_firmware_version\x18\xf4\x03 \x01(\x0b2#.focstim_rpc.RequestFirmwareVersionH\x00\x12H\n\x18request_capabilities_get\x18\xf5\x03 \x01(\x0b2#.focstim_rpc.RequestCapabilitiesGetH\x00\x12@\n\x14request_signal_start\x18\xf6\x03 \x01(\x0b2\x1f.focstim_rpc.RequestSignalStartH\x00\x12>\n\x13request_signal_stop\x18\xf7\x03 \x01(\x0b2\x1e.focstim_rpc.RequestSignalStopH\x00\x12>\n\x14request_axis_move_to\x18\x05 \x01(\x0b2\x1e.focstim_rpc.RequestAxisMoveToH\x00\x12B\n\x15request_timestamp_set\x18\xf8\x03 \x01(\x0b2 .focstim_rpc.RequestTimestampSetH\x00\x12B\n\x15request_timestamp_get\x18\xf9\x03 \x01(\x0b2 .focstim_rpc.RequestTimestampGetH\x00\x12M\n\x1brequest_wifi_parameters_set\x18\xfb\x03 \x01(\x0b2%.focstim_rpc.RequestWifiParametersSetH\x00\x12=\n\x13request_wifi_ip_get\x18\xfc\x03 \x01(\x0b2\x1d.focstim_rpc.RequestWifiIPGetH\x00\x12D\n\x16request_lsm6dsox_start\x18\xd8\x04 \x01(\x0b2!.focstim_rpc.RequestLSM6DSOXStartH\x00\x12B\n\x15request_lsm6dsox_stop\x18\xd9\x04 \x01(\x0b2 .focstim_rpc.RequestLSM6DSOXStopH\x00\x12R\n\x1erequest_debug_stm32_deep_sleep\x18\xe8\x07 \x01(\x0b2\'.focstim_rpc.RequestDebugStm32DeepSleepH\x00\x12S\n\x1erequest_debug_enter_bootloader\x18\xe9\x07 \x01(\x0b2(.focstim_rpc.RequestDebugEnterBootloaderH\x00B\x08\n\x06params"\xa5\x07\n\x08Response\x12\n\n\x02id\x18\x01 \x01(\r\x12J\n\x19response_firmware_version\x18\xf4\x03 \x01(\x0b2$.focstim_rpc.ResponseFirmwareVersionH\x00\x12J\n\x19response_capabilities_get\x18\xf5\x03 \x01(\x0b2$.focstim_rpc.ResponseCapabilitiesGetH\x00\x12B\n\x15response_signal_start\x18\xf6\x03 \x01(\x0b2 .focstim_rpc.ResponseSignalStartH\x00\x12@\n\x14response_signal_stop\x18\xf7\x03 \x01(\x0b2\x1f.focstim_rpc.ResponseSignalStopH\x00\x12@\n\x15response_axis_move_to\x18\x05 \x01(\x0b2\x1f.focstim_rpc.ResponseAxisMoveToH\x00\x12D\n\x16response_timestamp_set\x18\xf8\x03 \x01(\x0b2!.focstim_rpc.ResponseTimestampSetH\x00\x12D\n\x16response_timestamp_get\x18\xf9\x03 \x01(\x0b2!.focstim_rpc.ResponseTimestampGetH\x00\x12O\n\x1cresponse_wifi_parameters_set\x18\xfb\x03 \x01(\x0b2&.focstim_rpc.ResponseWifiParametersSetH\x00\x12?\n\x14response_wifi_ip_get\x18\xfc\x03 \x01(\x0b2\x1e.focstim_rpc.ResponseWifiIPGetH\x00\x12F\n\x17response_lsm6dsox_start\x18\xd8\x04 \x01(\x0b2".focstim_rpc.ResponseLSM6DSOXStartH\x00\x12D\n\x16response_lsm6dsox_stop\x18\xd9\x04 \x01(\x0b2!.focstim_rpc.ResponseLSM6DSOXStopH\x00\x12T\n\x1fresponse_debug_stm32_deep_sleep\x18\xe8\x07 \x01(\x0b2(.focstim_rpc.ResponseDebugStm32DeepSleepH\x00\x12!\n\x05error\x18\x03 \x01(\x0b2\x12.focstim_rpc.ErrorB\x08\n\x06result"*\n\x05Error\x12!\n\x04code\x18\x01 \x01(\x0e2\x13.focstim_rpc.Errors"\x9e\x01\n\nRpcMessage\x12\'\n\x07request\x18\x02 \x01(\x0b2\x14.focstim_rpc.RequestH\x00\x12)\n\x08response\x18\x04 \x01(\x0b2\x15.focstim_rpc.ResponseH\x00\x121\n\x0cnotification\x18\x05 \x01(\x0b2\x19.focstim_rpc.NotificationH\x00B\t\n\x07messageb\x06proto3')
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'focstim_rpc_pb2', _globals)
@@ -18,10 +18,10 @@ if not _descriptor._USE_C_DESCRIPTORS:
     _globals['_NOTIFICATION']._serialized_start = 89
     _globals['_NOTIFICATION']._serialized_end = 940
     _globals['_REQUEST']._serialized_start = 943
-    _globals['_REQUEST']._serialized_end = 1763
-    _globals['_RESPONSE']._serialized_start = 1766
-    _globals['_RESPONSE']._serialized_end = 2557
-    _globals['_ERROR']._serialized_start = 2559
-    _globals['_ERROR']._serialized_end = 2601
-    _globals['_RPCMESSAGE']._serialized_start = 2604
-    _globals['_RPCMESSAGE']._serialized_end = 2762
+    _globals['_REQUEST']._serialized_end = 1901
+    _globals['_RESPONSE']._serialized_start = 1904
+    _globals['_RESPONSE']._serialized_end = 2837
+    _globals['_ERROR']._serialized_start = 2839
+    _globals['_ERROR']._serialized_end = 2881
+    _globals['_RPCMESSAGE']._serialized_start = 2884
+    _globals['_RPCMESSAGE']._serialized_end = 3042

commit 84a1ac1cf3ab1f919c5976c3e87719ad1de31552

Restore smooth UI movement when using buttplug
This was broken in 5850fee

qt_ui/patterns/fourphase/mouse.py
@@ -27,3 +27,6 @@ class MousePattern(FourphasePattern):
 
     def update(self, dt: float):
         return self.x, self.y, self.z
+
+    def last_position_is_mouse_position(self):
+        return (self.x, self.y, self.z) == (self.alpha.last_value(), self.beta.last_value(), self.gamma.last_value())

qt_ui/patterns/fourphase_patterns.py
@@ -86,9 +86,14 @@ class FourphaseMotionGenerator(QtCore.QObject):
 
         if not self.any_scripts_loaded():
             if isinstance(self.pattern, MousePattern):
-                a = self.alpha.last_value()
-                b = self.beta.last_value()
-                c = self.gamma.last_value()
+                if self.pattern.last_position_is_mouse_position():
+                    a = self.alpha.last_value()
+                    b = self.beta.last_value()
+                    c = self.gamma.last_value()
+                else:
+                    a = self.alpha.interpolate(time.time() - self.latency)
+                    b = self.beta.interpolate(time.time() - self.latency)
+                    c = self.gamma.interpolate(time.time() - self.latency)
                 self.position_updated.emit(a, b, c)
             else:
                 a, b, c = self.pattern.update(dt * self.velocity)

qt_ui/patterns/threephase/mouse.py
@@ -24,3 +24,7 @@ class MousePattern(ThreephasePattern):
         self.y = y
     def update(self, dt: float):
         return self.x * self.amplitude, self.y * self.amplitude
+    def last_position_is_mouse_position(self):
+        if self.alpha is not None and self.beta is not None:
+            return (self.x, self.y) == (self.alpha.last_value(), self.beta.last_value())
+        return False

qt_ui/patterns/threephase_patterns.py
@@ -101,8 +101,12 @@ class ThreephaseMotionGenerator(QtCore.QObject):
 
         if not self.any_scripts_loaded():
             if isinstance(self.pattern, MousePattern):
-                a = self.alpha.last_value()
-                b = self.beta.last_value()
+                if self.pattern.last_position_is_mouse_position():
+                    a = self.alpha.last_value()
+                    b = self.beta.last_value()
+                else:
+                    a = self.alpha.interpolate(time.time() - self.latency)
+                    b = self.beta.interpolate(time.time() - self.latency)
                 self.position_updated.emit(a, b)
             else:
                 a, b = self.pattern.update(dt * self.velocity)